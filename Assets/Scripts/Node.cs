25103637353d9456935627e0d873a7c5b6e92cfc
itstor <ahmdthoriq5@gmail.com>
2021-11-25 11:50:41 +0700

    change NodeProperty to Node add Add connected node

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -2,21 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NodeProperty : MonoBehaviour
+public class Node : MonoBehaviour
 {
     public string nodeName;
     private TMPro.TextMeshPro _textMesh;
-    public List<NodeProperty> connectedNodes;
+    public Dictionary<Node, float> connectedNodes;
     // Start is called before the first frame update
     void Start()
     {
         _textMesh = gameObject.GetComponentInChildren<TMPro.TextMeshPro>();
         _textMesh.text = nodeName;
+        connectedNodes = new Dictionary<Node, float>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Add(ref Node node, float distance)
+    {
+        connectedNodes.Add(node, distance);
     }
 }
