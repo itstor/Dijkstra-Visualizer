ec8ff5af974bcd024bef71befff96a0839ce5333
itstor <ahmdthoriq5@gmail.com>
2021-11-30 19:02:11 +0700

    added states namespace

---
+++ Assets/Scripts/States.cs
@@ -0,0 +1,15 @@
+namespace states {
+    public enum CursorState
+    {
+        Select,
+        Connect,
+        Add,
+    }
+
+    public enum ToolType
+    {
+        Select,
+        Connect,
+        Add,
+    }
+}
